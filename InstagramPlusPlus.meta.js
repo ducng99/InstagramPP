7a0bbcd4a9d359b9126649dea6a1cdc358158e07
Duc Nguyen <tuanduc249.td@gmail.com>
2020-10-23 01:20:16 +1300

    Update InstagramPlusPlus.meta.js

--- InstagramPlusPlus.meta.js
+++ InstagramPlusPlus.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram++
 // @namespace    maxhyt.instagrampp
-// @version      3.4
+// @version      3.4.1
 // @updateURL    https://ducng99.github.io/InstagramPP/InstagramPlusPlus.meta.js
 // @downloadURL  https://ducng99.github.io/InstagramPP/InstagramPlusPlus.user.js
 // ==/UserScript==
