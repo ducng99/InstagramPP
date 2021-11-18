7160c4d4634f22e0e60207d6d25f46c4d8c80b6b
Thomas Nguyen <tom@ducng.dev>
2021-11-18 23:02:23 +1300

    Update InstagramPlusPlus.meta.js

--- InstagramPlusPlus.meta.js
+++ InstagramPlusPlus.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram++
 // @namespace    maxhyt.instagrampp
-// @version      3.5.2
+// @version      3.5.3
 // @updateURL    https://ducng99.github.io/InstagramPP/InstagramPlusPlus.meta.js
 // @downloadURL  https://ducng99.github.io/InstagramPP/InstagramPlusPlus.user.js
 // ==/UserScript==
