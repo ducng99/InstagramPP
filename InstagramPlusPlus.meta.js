448f449733ba4977f5e973bc48a7a9d39ba22e9f
Thomas Nguyen <tom@ducng.dev>
2021-11-18 23:08:55 +1300

    Delete InstagramPlusPlus.meta.js

--- InstagramPlusPlus.meta.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name         Instagram++
-// @namespace    maxhyt.instagrampp
-// @version      3.5.3
-// @updateURL    https://ducng99.github.io/InstagramPP/InstagramPlusPlus.meta.js
-// @downloadURL  https://ducng99.github.io/InstagramPP/InstagramPlusPlus.user.js
-// ==/UserScript==
