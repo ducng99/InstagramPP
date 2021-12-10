384419f1d49f6498483a3863f0c0e16e25027ddd
Thomas Nguyen <tom@ducng.dev>
2021-12-10 16:52:19 +1300

    v3.8.1
    Refactoring

--- InstagramPlusPlus.user.js
+++ InstagramPlusPlus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram++
 // @namespace    maxhyt.instagrampp
-// @version      3.8.0
+// @version      3.8.1
 // @description  Add addtional features to Instagram
 // @author       Maxhyt
 // @license      GPL-3.0
@@ -19,9 +19,9 @@
     function MainLoop() {
         //Story        
         let storyMenu = document.querySelector("._8p8kF");
-        if (storyMenu !== null && !storyMenu.querySelector('.igpp_download')) {
+        if (storyMenu && !storyMenu.querySelector('.igpp_download')) {
             const newNode = document.createElement('div');
-            newNode.innerHTML = `<button class="wpO6b igpp_download" type="button"><div class="QBdPU"><svg width="16" height="16" fill="#ffffff" color="#ffffff" class="_8-yf5" viewBox="0 0 16 16"><path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z"/></svg></div></button>`;
+            newNode.innerHTML = '<button class="wpO6b igpp_download" type="button"><div class="QBdPU"><svg width="18" height="18" fill="#ffffff" color="#ffffff" class="_8-yf5" viewBox="0 0 16 16"><path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z"/></svg></div></button>';
             const downloadButton = newNode.firstChild;
             downloadButton.addEventListener('click', DownloadStory);
             storyMenu.insertBefore(downloadButton, storyMenu.firstChild);
@@ -36,7 +36,6 @@
     function DownloadStory() {
         let stPicLink = document.body.querySelector("img.y-yJ5")?.getAttribute("srcset")?.split(" ")[0];
         let stVidLink = document.body.querySelector("video.y-yJ5.OFkrO")?.querySelector("source")?.getAttribute("src");
-        let downloadDOM = document.createElement('a');
 
         if (stVidLink) {
             window.open(stVidLink, '_blank');
@@ -50,72 +49,59 @@
     }
 
     async function ProcessArticle(article) {
-        let feedMenu = article.querySelector(".ltpMr.Slqrh");
+        let feedMenu = article.querySelector('.ltpMr.Slqrh');
 
         if (!feedMenu.querySelector('.igpp_download')) {
-            const mediaCount = article.querySelector("div._3eoV-.IjCL9");
-            const src = GetMediaSrc(article, mediaCount);
+            const src = GetMediaSrc(article);
 
-            if (!src) {
-                return;
-            }
+            if (src) {
+                let arrowSwitchLeft = article.querySelector('.coreSpriteLeftChevron');
+                if (arrowSwitchLeft) {
+                    arrowSwitchLeft.addEventListener('click', () => { ResetDownloadLink(article, 100); });
+                }
 
-            let arrowArticleLeft = document.body.querySelector(".coreSpriteLeftPaginationArrow");
-            if (arrowArticleLeft !== null) {
-                arrowArticleLeft.addEventListener('click', () => { ResetDownloadLink(document, 100); });
-            }
+                let arrowSwitchRight = article.querySelector('.coreSpriteRightChevron');
+                if (arrowSwitchRight) {
+                    arrowSwitchRight.addEventListener('click', () => { ResetDownloadLink(article, 100); });
+                }
 
-            let arrowArticleRight = document.body.querySelector(".coreSpriteRightPaginationArrow");
-            if (arrowArticleRight !== null) {
-                arrowArticleRight.addEventListener('click', () => { ResetDownloadLink(document, 100); });
+                let newNode = document.createElement("div");
+                newNode.innerHTML = `<span class="igpp_download"><a class="wpO6b" href="${src}" target="_blank"><div class="QBdPU"><svg class="_8-yf5" width="24" height="24" viewBox="0 0 16 16" color="#262626" fill="#262626" aria-label="Download"><path fill-rule="evenodd" d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg></div></a></span>`;
+                feedMenu.appendChild(newNode.firstChild);
             }
-
-            let arrowSwitchLeft = article.querySelector(".coreSpriteLeftChevron");
-            if (arrowSwitchLeft !== null) {
-                arrowSwitchLeft.addEventListener('click', () => { ResetDownloadLink(article, 100); });
-            }
-
-            let arrowSwitchRight = article.querySelector(".coreSpriteRightChevron");
-            if (arrowSwitchRight !== null) {
-                arrowSwitchRight.addEventListener('click', () => { ResetDownloadLink(article, 100); });
-            }
-            
-            let newNode = document.createElement("div");
-            newNode.innerHTML = '<span class="igpp_download"><a class="wpO6b" href="' + src + '" target="_blank"><div class="QBdPU"><svg class="_8-yf5" width="24" height="24" viewBox="0 0 16 16" fill="#262626" aria-label="Download"><path fill-rule="evenodd" d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg></div></a></span>';
-            feedMenu.appendChild(newNode.firstChild);
         }
     }
 
-    function GetMediaSrc(article, mediaCountDOM) {
+    function GetMediaSrc(article) {
         let mediaIndex = -1;
-        let src = '';
+        const mediaCountDOM = article.querySelector("div._3eoV-.IjCL9");
 
         if (mediaCountDOM && mediaCountDOM.children.length > 1) {
             let current = mediaCountDOM.querySelector(".Yi5aA.XCodT");
             mediaIndex = [...mediaCountDOM.children].indexOf(current);
         }
 
-        let dateDOM = article.querySelector(".NnvRN > a");
+        const dateDOM = article.querySelector(".NnvRN > a");
         if (dateDOM) {
             for (const links of CapturedMediaURLs) {
                 if (dateDOM.href.includes(links.postID)) {
                     if (mediaIndex === -1) {
-                        src = links.src;
+                        return links.src;
                     }
                     else {
-                        src = links.srcs[mediaIndex];
+                        return links.srcs[mediaIndex];
                     }
                 }
             }
         }
 
-        return src;
+        return null;
     }
 
     function ResetDownloadLink(article, timeout) {
         setTimeout(() => {
-            article.querySelector(".igpp_download").remove();
-            MainLoop();
+            article.querySelector(".igpp_download")?.remove();
+            ProcessArticle(article);
         }, timeout);
     }
 
@@ -136,7 +122,7 @@
                 else if (event.target.responseURL.includes("https://www.instagram.com/graphql/query/")) {
                     const media = response.data.shortcode_media;
                     if (media) {
-                        ProcessMediaObjFromGraphQL(media);
+                        ParseMediaObjFromGraphQL(media);
                     }
                 }
             }, false);
@@ -154,7 +140,7 @@
                     if (matches[1].includes("graphql")) {
                         let media = JSON.parse(matches[1])?.graphql?.shortcode_media;
                         if (media) {
-                            ProcessMediaObjFromGraphQL(media);
+                            ParseMediaObjFromGraphQL(media);
                         }
                     }
                     else if (matches[1].includes("feed_items")) {
@@ -168,14 +154,14 @@
         });
     });
 
-    function ProcessMediaObjFromGraphQL(media, save = true) {
+    function ParseMediaObjFromGraphQL(media, save = true) {
         const postID = media.shortcode;
 
         if (media.__typename === "GraphSidecar") {
             let links = [];
 
             media.edge_sidecar_to_children.edges.forEach(edge => {
-                let link = ProcessMediaObjFromGraphQL(edge.node, false);
+                let link = ParseMediaObjFromGraphQL(edge.node, false);
                 links.push(link.src);
             });
 
