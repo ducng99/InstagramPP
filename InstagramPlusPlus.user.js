fe0c6c61e8cc42a2481bd06fe1b6fc90940495c6
Thomas Nguyen <tom@ducng.dev>
2022-03-20 15:50:26 +1300

    v4.0.0

--- InstagramPlusPlus.user.js
+++ InstagramPlusPlus.user.js
@@ -1,20 +1,36 @@
 // ==UserScript==
 // @name         Instagram++
 // @namespace    maxhyt.instagrampp
-// @version      3.8.8
+// @version      4.0.0
 // @description  Add addtional features to Instagram
 // @author       Maxhyt
 // @license      AGPL-3.0
 // @icon         https://icons.duckduckgo.com/ip2/instagram.com.ico
 // @homepage     https://github.com/ducng99/InstagramPP
 // @match        https://www.instagram.com/*
+// @match        https://beta.ducng.dev/InstagramPP/
+// @require      https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js
 // @run-at       document-start
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_openInTab
+// @grant        unsafeWindow
 // ==/UserScript==
 
 (function () {
+    "use strict"
+    
+    const STORAGE_VARS = { BlockSeenStory: "block_seen_story", AutoReportSpamComments: "auto_report_spam_comments",
+                          ReportedComments: "reported_comments", CheckedComments: "checked_comments" };
     let CapturedMediaURLs = [];
+    let ReportCommentsQueue = [];
+    const SETTINGS_PAGE = "https://beta.ducng.dev/InstagramPP/";
+    
+    LoadSettings();
 
     setInterval(MainLoop, 2000);
+    ReportLoop();
 
     function MainLoop() {
         // Story
@@ -78,6 +94,43 @@
             arrowSwitchRight.addEventListener('click', () => { ResetDownloadLink(article, 100); });
             arrowSwitchRight.classList.add('igpp_checked');
         }
+        
+        if (GM_getValue(STORAGE_VARS.AutoReportSpamComments)) {
+            const list_comments = article.querySelectorAll('ul.XQXOT.pXf-y > ul.Mr508');
+            const toBeCheckedComments = {};
+            const IDsToElement = {};
+            
+            list_comments.forEach(comment_container => {
+                if (!comment_container.hasAttribute("igpp_checked")) {
+                    comment_container.setAttribute("igpp_checked", "");
+
+                    const commentText = comment_container.querySelector('.MOdxS').textContent;
+                    const timeLink = comment_container.querySelector('a.gU-I7');
+                    const match = /\/p\/[a-z0-9]+\/c\/(\d+)/i.exec(timeLink.href);
+
+                    if (timeLink && match) {
+                        if (GetReportedComments().indexOf(match[1]) === -1) {
+                            toBeCheckedComments[match[1]] = commentText;
+                            IDsToElement[match[1]] = comment_container;
+                        }
+                        else {
+                            comment_container.remove();
+                        }
+                    }
+                }
+            });
+            
+            const checkedCommentsResult = Object.entries(await CheckSpamComments(toBeCheckedComments));
+            
+            checkedCommentsResult.forEach(comment => {
+                if (comment[1]) {
+                    if (IDsToElement[comment[0]]) {
+                        IDsToElement[comment[0]].remove();
+                    }
+                    AddReportCommentID(comment[0]);
+                }
+            });
+        }
     }
 
     function GetMediaSrc(article) {
@@ -117,49 +170,51 @@
 
     // Overwrite the native method
     XMLHttpRequest.prototype.open = function () {
-        if (!arguments[1].includes("/stories/reel/seen")) {
-            // Assign an event listener
-            this.addEventListener("load", event => {
-                let response = JSON.parse(event.target.responseText);
-
-                if (event.target.responseURL === "https://i.instagram.com/api/v1/feed/timeline/") {
-                    response.feed_items.forEach(item => {
-                        ParseMediaObjFromAPI(item.media_or_ad);
-                    });
-                }
-                else if (event.target.responseURL.includes("https://www.instagram.com/graphql/query/")) {
-                    const media = response.data.user?.edge_owner_to_timeline_media.edges;
-                    if (media) {
-                        media.forEach(edge => ParseMediaObjFromGraphQL(edge.node));
-                    }
-                }
-                else if (event.target.responseURL.includes("https://www.instagram.com/explore/grid/")) {
-                    let sections = response.sectional_items;
-                    
-                    sections.forEach(section => {
-                        if (section.layout_type === "media_grid") {
-                            section.layout_content.medias.forEach(media => ParseMediaObjFromAPI(media.media));
-                        }
-                        else if (section.layout_type.startsWith('two_by_two')) {
-                            if (section.layout_content.two_by_two_item.channel) {
-                                ParseMediaObjFromAPI(section.layout_content.two_by_two_item.channel.media);
-                            }
-                            else {
-                                ParseMediaObjFromAPI(section.layout_content.two_by_two_item.media);
-                            }
-                            section.layout_content.fill_items.forEach(item => ParseMediaObjFromAPI(item.media));
-                        }
-                    });
-                }
-                else if (event.target.responseURL.includes("https://i.instagram.com/api/v1/media/")) {
-                    if (response.items && response.items[0]) {
-                        ParseMediaObjFromAPI(response.items[0]);
-                    }
-                }
-            }, false);
-            // Call the stored reference to the native method
-            XHR_open.apply(this, arguments);
+        if (GM_getValue(STORAGE_VARS.BlockSeenStory) && arguments[1].includes("/stories/reel/seen")) {
+            return;
         }
+        
+        // Assign an event listener
+        this.addEventListener("load", event => {
+            let response = JSON.parse(event.target.responseText);
+
+            if (event.target.responseURL === "https://i.instagram.com/api/v1/feed/timeline/") {
+                response.feed_items.forEach(item => {
+                    ParseMediaObjFromAPI(item.media_or_ad);
+                });
+            }
+            else if (event.target.responseURL.includes("https://www.instagram.com/graphql/query/")) {
+                const media = response.data.user?.edge_owner_to_timeline_media.edges;
+                if (media) {
+                    media.forEach(edge => ParseMediaObjFromGraphQL(edge.node));
+                }
+            }
+            else if (event.target.responseURL.includes("https://www.instagram.com/explore/grid/")) {
+                let sections = response.sectional_items;
+
+                sections.forEach(section => {
+                    if (section.layout_type === "media_grid") {
+                        section.layout_content.medias.forEach(media => ParseMediaObjFromAPI(media.media));
+                    }
+                    else if (section.layout_type.startsWith('two_by_two')) {
+                        if (section.layout_content.two_by_two_item.channel) {
+                            ParseMediaObjFromAPI(section.layout_content.two_by_two_item.channel.media);
+                        }
+                        else {
+                            ParseMediaObjFromAPI(section.layout_content.two_by_two_item.media);
+                        }
+                        section.layout_content.fill_items.forEach(item => ParseMediaObjFromAPI(item.media));
+                    }
+                });
+            }
+            else if (event.target.responseURL.includes("https://i.instagram.com/api/v1/media/")) {
+                if (response.items && response.items[0]) {
+                    ParseMediaObjFromAPI(response.items[0]);
+                }
+            }
+        }, false);
+        // Call the stored reference to the native method
+        XHR_open.apply(this, arguments);
     };
 
     window.addEventListener('load', () => {
@@ -237,5 +292,142 @@
             if (save) CapturedMediaURLs.push({ postID, src });
             return { postID, src };
         }
+    }
+    
+    /* START - REPORT SPAM SECTION */
+    
+    async function ReportLoop() {
+        while (GM_getValue(STORAGE_VARS.AutoReportSpamComments, false)) {
+            const tmp_ReportCommentsQueue = [...ReportCommentsQueue];
+            for (let i = 0; i < tmp_ReportCommentsQueue.length; i++) {
+                const id = tmp_ReportCommentsQueue[i];
+                ReportCommentsQueue.splice(ReportCommentsQueue.indexOf(id), 1);
+                if (await SendReport(id)) {
+                    AddReportedComment(id);
+                }
+                await Sleep(2000);
+            }
+            
+            await Sleep(2000);
+        }
+    }
+    
+    async function CheckSpamComments(comments) {
+        if (Object.keys(comments).length > 0)
+        {
+            const res = await fetch("https://4ze85p5fce.execute-api.ap-southeast-2.amazonaws.com/v1/IsInstagramCommentSpam", {
+                body: JSON.stringify(comments),
+                method: 'POST'
+            });
+
+            return await res.json();
+        }
+        
+        return {};
+    }
+    
+    async function SendReport(comment_id) {
+        const requestForm = new FormData;
+        requestForm.append("entry_point", "1");
+        requestForm.append("location", "3");
+        requestForm.append("object_type", "2");
+        requestForm.append("object_id", comment_id);
+        requestForm.append("container_module", "postPage");
+        requestForm.append("frx_prompt_request_type", "1");
+        
+        try {
+            let res_report_request = await fetch("https://www.instagram.com/reports/web/get_frx_prompt/", {
+                body: new URLSearchParams(requestForm),
+                method: 'POST',
+                headers: {
+                    "X-CSRFToken": Cookies.get('csrftoken'),
+                    "X-Instagram-AJAX": unsafeWindow._sharedData.rollout_hash,
+                    "X-IG-App-ID": 936619743392459
+                }
+            });
+
+            res_report_request = await res_report_request.json();
+
+            if (res_report_request.status === "ok") {
+                const context = res_report_request.response.context;
+
+                const reportForm = new FormData;
+                reportForm.append("context", context);
+                reportForm.append("selected_tag_type", "ig_spam_v3");
+
+                let res_report = await fetch("https://www.instagram.com/reports/web/log_tag_selected/", {
+                    body: new URLSearchParams(reportForm),
+                    method: 'POST',
+                    headers: {
+                        "X-CSRFToken": Cookies.get('csrftoken'),
+                        "X-Instagram-AJAX": unsafeWindow._sharedData.rollout_hash,
+                        "X-IG-App-ID": 936619743392459
+                    }
+                });
+                res_report = await res_report.json();
+
+                if (res_report.status === "ok") {
+                    console.log(`Report ${comment_id} success`);
+                    return true;
+                }
+                else {
+                    console.error("Failed to send report", res_report);
+                }
+            }
+            else {
+                console.error("Failed to send report request", res_report_request);
+            }
+        }
+        catch (ex) {
+            console.error("Failed send report", ex);
+        }
+        
+        return false;
+    }
+    
+    function AddReportCommentID(id) {
+        ReportCommentsQueue = [...new Set([...ReportCommentsQueue, id])];
+    }
+    
+    function GetReportedComments() {
+        return JSON.parse(GM_getValue(STORAGE_VARS.ReportedComments, "[]"));
+    }
+    
+    function AddReportedComment(id) {
+        let storedIDs = GetReportedComments();
+        GM_setValue(STORAGE_VARS.ReportedComments, JSON.stringify([...new Set([...storedIDs, id])]));
+    }
+    
+    /* END - REPORT SPAM SECTION */
+    
+    // Open settings page
+    GM_registerMenuCommand("Settings", () => window.open(SETTINGS_PAGE, "_blank"));
+    
+    function LoadSettings() {
+        // Set default settings if not exists
+        if (GM_getValue(STORAGE_VARS.BlockSeenStory, null) === null) {
+            GM_setValue(STORAGE_VARS.BlockSeenStory, true);
+        }
+        
+        if (GM_getValue(STORAGE_VARS.AutoReportSpamComments, null) === null) {
+            GM_setValue(STORAGE_VARS.AutoReportSpamComments, false);
+        }
+        
+        // Setup settings page
+        if (window.location.href.includes(SETTINGS_PAGE)) {
+            window.addEventListener('load', () => {
+                document.getElementById(STORAGE_VARS.BlockSeenStory).checked = GM_getValue(STORAGE_VARS.BlockSeenStory);
+                document.getElementById(STORAGE_VARS.AutoReportSpamComments).checked = GM_getValue(STORAGE_VARS.AutoReportSpamComments);
+                
+                document.querySelector("#save_settings").addEventListener('click', () => {
+                    GM_setValue(STORAGE_VARS.BlockSeenStory, document.getElementById(STORAGE_VARS.BlockSeenStory).checked);
+                    GM_setValue(STORAGE_VARS.AutoReportSpamComments, document.getElementById(STORAGE_VARS.AutoReportSpamComments).checked);
+                });
+            });
+        }
+    }
+    
+    function Sleep(time) {
+        return new Promise(resolve => setTimeout(() => resolve(), time));
     }
 })();
